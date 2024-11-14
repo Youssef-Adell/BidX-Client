843c9def44c9934210db41cb38e6ea6c3ff36f4c
unknown <al3nkpout2@gmail.com>
2024-11-15 01:47:52 +0200

    Improve SignalR performance by skipping the negotiate request which reduces app startup time too

--- src/api/signalRConnection.js
+++ src/api/signalRConnection.js
@@ -4,6 +4,8 @@ import { useAuthStore } from "@/stores/AuthStore";
 const signalRConnection = new signalR.HubConnectionBuilder()
   .withUrl("https://bidx.runasp.net/appHub", {
     accessTokenFactory: () => useAuthStore().accessToken,
+    transport: signalR.HttpTransportType.WebSockets,
+    skipNegotiation: true, // Improves the performance by skipping the negotiate request and establish the WS connection directly
   })
   .configureLogging(signalR.LogLevel.Information)
   .withAutomaticReconnect()
