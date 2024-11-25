112ebba651fc7f0ffea80864f56140d332605853
Youssef Adel <al3nkpout2@gmail.com>
2024-11-25 23:45:18 +0200

    Retry SignalR connection on failure for a max of 5 attempts in startConnection()
    to ensure that the app won't start unless the SignalR connection is established, and ensure that the signalR connection is restarted successfully when user login or logout

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,53 @@
 import ConnectionStateBar from "./components/ConnectionStateBar.vue";
 import Footer from "./components/Footer.vue";
 import Navbar from "./components/Navbar.vue";
+import signalrClient from "./api/signalrClient";
 import { useTheme } from "vuetify";
+import { useAuthStore } from "./stores/AuthStore";
+import { onBeforeMount, onBeforeUnmount, ref } from "vue";
 
+const authStore = useAuthStore();
 const theme = useTheme();
-const selectedTheme = localStorage.getItem("selectedTheme");
-if (selectedTheme) theme.global.name.value = selectedTheme;
+
+const isAppInitialized = ref(false);
+const isAppInitializationFailed = ref(false);
+
+onBeforeMount(async () => {
+  const selectedTheme = localStorage.getItem("selectedTheme");
+  if (selectedTheme) theme.global.name.value = selectedTheme;
+
+  try {
+    await authStore.refreshToken();
+    await signalrClient.startConnection();
+    isAppInitialized.value = true;
+  } catch {
+    isAppInitializationFailed.value = true;
+  }
+});
+
+onBeforeUnmount(() => {
+  signalrClient.stopConnection();
+});
 </script>
 
 <template>
   <VApp>
-    <ConnectionStateBar />
-    <Navbar />
-    <VMain>
-      <RouterView />
-    </VMain>
-    <Footer />
+    <template v-if="!isAppInitialized">
+      <div class="d-flex align-center justify-center text-center h-100">
+        <div v-if="isAppInitializationFailed">
+          Connection failed!<br />Please reload the page.
+        </div>
+        <div v-else>Loading...</div>
+      </div>
+    </template>
+
+    <template v-else>
+      <ConnectionStateBar />
+      <Navbar />
+      <VMain>
+        <RouterView />
+      </VMain>
+      <Footer />
+    </template>
   </VApp>
 </template>
--- src/main.js
+++ src/main.js
@@ -8,24 +8,11 @@ import App from "./App.vue";
 
 // Composables
 import { createApp } from "vue";
-import { useAuthStore } from "./stores/AuthStore";
-import signalrClient from "./api/signalrClient";
 
-async function initializeApp() {
-  const app = createApp(App);
+const app = createApp(App);
 
-  app.use(vuetify);
-  app.use(pinia);
+app.use(vuetify);
+app.use(pinia);
+app.use(router);
 
-  // Intialize the state of authStore before registering router plugin because navigation guards & signalR depends on it
-  const authStore = useAuthStore();
-  await authStore.refreshToken();
-
-  await signalrClient.startConnection();
-  app.onUnmount(() => signalrClient.stopConnection());
-
-  app.use(router);
-  app.mount("#app");
-}
-
-initializeApp();
+app.mount("#app");
