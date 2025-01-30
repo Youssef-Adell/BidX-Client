9ace75ec79688e28264345ddbb3a062de7658616
Youssef Adel <al3nkpout2@gmail.com>
2025-01-30 15:30:51 +0200

    Add notification sound

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -3,7 +3,7 @@ import { addAuthHeader, normalizeErrorResponse } from "@/utils/apiUtils";
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: "http://localhost:5203/api",
+  baseURL: "http://192.168.1.10:9090/api",
 });
 
 // Optionally add the Authorization header based on whether the request requires authentication
